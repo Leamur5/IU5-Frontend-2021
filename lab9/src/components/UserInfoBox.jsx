c87b1148e77c67ed9cf7d80c80a77dd691fce6a6
Leamur5 <leamur5@mail.ru>
2021-04-28 21:47:56 +0300

    bozhe hrani Vadima

---
+++ lab9/src/components/UserInfoBox.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import './UserInfoBox.css';
+
+const UserInfoBox = ({ info }) => { 
+    const history = useHistory();
+    const backClick = () => {
+        history.push(process.env.REACT_APP_DEV === "true" ? "/" : "/lab9/build/");
+    };
+    return(
+    
+        <div className="user-info-box">
+            <img src={info.avatar_url} alt={info.login} className="avatar" />
+            <h1 className="login">{info.login}</h1>
+            <h2 className="name">{info.name}</h2>
+            <div className="followers">
+      
+            {info.followers} followers &#129309; {info.following} following &#128064;
+        </div>
+        {info.location ? (
+        <div className="location">
+        
+            {info.location}
+        </div>) : (<></>)}
+        {info.blog ? (
+        <div>
+        
+            <a href={info.blog} rel="noreferrer" target="_blank" id="blog-info"className="user-link">
+                {info.blog}
+            </a>
+        </div>
+        ) : (
+        <></>)}
+        <a href={info.html_url} rel="noreferrer" target="_blank" className="user-link">
+            Ссылка на GitHub
+        </a>
+        <button type="button" onClick={backClick} className="back-button">
+            Ищем другого!
+        </button>
+  </div>
+);
+}
+
+export default UserInfoBox;
